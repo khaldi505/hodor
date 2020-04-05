824779cb74eac0e15249ee6669872018141cf6c6
khaldi505 <habetlbrika69@gmail.com>
2020-04-05 09:22:48 +0200

    fi

--- level_0/main.py
+++ level_0/main.py
@@ -1,20 +1,11 @@
 #!/usr/bin/python3
+#cheat vote is fun
 import requests
 
-print("hello world!\n")
 url = "http://158.69.76.135/level0.php"
 id = int(input("enter your id:\n"))
-yes_no = input("wanna see the request status y/n ?")
 for req in range(1024):
-
     requests.post(url, data={
         'id': id,
         'holdthedoor': "Submit"
     })
-    if yes_no == 'y':
-        print("request number {} has been sent to {}".format(req, url))
-    elif yes_no == 'n':
-        pass
-    else:
-        print("please enter y or n")
-print("done!")
