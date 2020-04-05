239f1a721c1fd2253f04205348356aeef6a9651c
khaldi505 <habetlbrika69@gmail.com>
2020-04-05 09:04:30 +0200

    fixed

--- level_0/main.py
+++ level_0/main.py
@@ -1,11 +1,20 @@
 #!/usr/bin/python3
 import requests
-url = "http://158.69.76.135/level0.php"
-id = 1129
 
+print("hello world!\n")
+url = "http://158.69.76.135/level0.php"
+id = int(input("enter your id:\n"))
+yes_no = input("wanna see the request status y/n ?")
 for req in range(1024):
-    requests.post(url, allow_redirects=False, data={
+
+    requests.post(url, data={
         'id': id,
         'holdthedoor': "Submit"
     })
-    print("request number {} has been sent to {}".format(req, url))
+    if yes_no == 'y':
+        print("request number {} has been sent to {}".format(req, url))
+    elif yes_no == 'n':
+        pass
+    else:
+        print("please enter y or n")
+print("done!")
