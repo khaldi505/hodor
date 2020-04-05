e3dfd43aa77fb780309b0eb2f672206414df2397
khaldi505 <habetlbrika69@gmail.com>
2020-04-05 22:58:41 +0200

    main.py

--- level_0/main.py
+++ level_0/main.py
@@ -1,13 +1,15 @@
 #!/usr/bin/python3
 # cheat vote is fun
 import requests
-with requests.Session() as x:
-    url = "http://158.69.76.135/level0.php"
-    x.get(url)
+url = "http://158.69.76.135/level0.php"
+id = str(1129)
+data_2_post = {
+        'id': id,
+        'holdthedoor': "Submit"
+    }
 print("please wait ..")
 for req in range(1024):
-    requests.post(url, data={
-        'id': 1129,
-        'holdthedoor': "Submit"
-    })
+    requests.post(url, data= data_2_post)
+    print("request number {} has been sent to {} with the id: {}".format(req, url, id))
+
 print("done")
