0b1cac3d0f78028963c845c4ebc3065effedd48b
khaldi505 <habetlbrika69@gmail.com>
2020-04-05 22:36:18 +0200

    main.py updated

--- level_0/main.py
+++ level_0/main.py
@@ -1,11 +1,13 @@
 #!/usr/bin/python3
 # cheat vote is fun
 import requests
-
-url = "http://158.69.76.135/level0.php"
-id = 1129
+with requests.Session() as x:
+    url = "http://158.69.76.135/level0.php"
+    x.get(url)
+print("please wait ..")
 for req in range(1024):
     requests.post(url, data={
-        'id': id,
+        'id': 1129,
         'holdthedoor': "Submit"
     })
+print("done")
