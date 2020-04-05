fe614845ae14f81febb884a923d137840b1f222b
khaldi505 <habetlbrika69@gmail.com>
2020-04-05 09:24:23 +0200

    fixed

--- level_0/main.py
+++ level_0/main.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
-#cheat vote is fun
+# cheat vote is fun
 import requests
 
 url = "http://158.69.76.135/level0.php"
-id = int(input("enter your id:\n"))
+id = 1129
 for req in range(1024):
     requests.post(url, data={
         'id': id,
